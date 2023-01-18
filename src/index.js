459fcebfe10235018516ae43e23b53125ad66602
sumittricon <sumit.anshul@triconinfotech.com>
2023-01-18 11:56:47 +0530

    Added Project Files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const path = require("path");
+const hbs  = require("hbs");
 const app = express();
 const port = process.env.PORT || 8000;
 
 
 //public static path
 const staticPath = path.join(__dirname, "../public");
-
+const templatePath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
 
 app.set("view engine", "hbs");
+
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
+
 app.use(express.static(staticPath));
 
 
